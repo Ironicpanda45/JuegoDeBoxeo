665edf7cb9461aebdd731e42286b1b7320558a2b
Ironicpanda45 <Leiruzielrp@gmail.com>
2025-11-26 19:24:35 -0700

    lol

--- Assets/Scripts/Cinematicas/CambiarEstadoAnimado.cs
+++ Assets/Scripts/Cinematicas/CambiarEstadoAnimado.cs
@@ -8,4 +8,7 @@ public class CambiarEstadoAnimado : MonoBehaviour
     public void SetEstado0() { anim.SetInteger("Estado", 0); }
     public void SetEstado1() { anim.SetInteger("Estado", 1); }
     public void SetEstado2() { anim.SetInteger("Estado", 2); }
+    public void SetEstado3() { anim.SetInteger("Estado", 3); }
+    public void SetEstado4() { anim.SetInteger("Estado", 4); }
+    public void SetEstado5() { anim.SetInteger("Estado", 5); }
 }
